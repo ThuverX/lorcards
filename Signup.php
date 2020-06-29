6745909679e0cb97391ba9cfb9ead60c0d6890aa
CherenX <jl.wagenmakers@student.alfa-college.nl>
2020-06-29 15:50:53 +0200

    register gemaakt

---
+++ Signup.php
@@ -0,0 +1,26 @@
+<?php
+if (isset($_POST['signup-submit'])) {
+    
+    require 'connect.php';
+
+    $username = $_POST[''];
+    $email = $_POST[''];
+    $password = $_POST[''];
+    $passwordRepeat = $_POST[''];
+    
+    if ($password !== $passwordRepeat) {
+        echo "password does not equal repeat password"
+        return
+    }
+    else {
+        $hashedPwd = password_hash($password, PASSWORD_BCRYPT);
+        $sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->excecute(['username' => $username, 'password' => $hashedPwd, 'email' => $email])
+        $_SESSION['user'] = $username
+        header('location: localhost/lorcards/');
+    }
+
+
+}
+?>
---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername ="localhost";
+$dBUsername ="root";
+$dBPassword ="";
+$dBName ="lorcards";
+
+$conn = new PDO("mysql:host=$servername;dbname=$dBName", $dBUsername, $dBPassword);
+
+?>
